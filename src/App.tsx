27128cc52c6bb716827fbe2ba8d39bc2680a8e30
Pontus Doverstav <doverstav@gmail.com>
2021-10-03 19:23:14 +0200

    Add more customization

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { walk } from './pieces/bishop/bishop';
 import { Path } from './pieces/types';
-import { StringBoard } from './board/StringBoard';
+import { BoardProps, StringBoard } from './board/StringBoard';
 
 function App() {
   const [walkResult, setWalkResult] = useState<Path>([]);
-  const boardHeight = 9;
-  const boardWidth = 17;
+  const [boardHeight, setBoardHeight] = useState(9);
+  const [boardWidth, setBoardWidth] = useState(17);
+  const [input, setInput] = useState("test input")
+  const [stringBoardProps, setStringBoardProps] = useState<BoardProps>({
+    height: boardHeight,
+    width: boardWidth,
+    paths: [walkResult]
+  })
+
+  useEffect(() => {
+    const walkResult = walk({ boardHeight, boardWidth, input })
+    setStringBoardProps({ height: boardHeight, width: boardWidth, paths: [walkResult] })
+  }, [boardHeight, boardWidth, input])
 
   return (
     <div className="App">
-      <button onClick={() => setWalkResult(walk({ boardHeight, boardWidth, input: "test input" }))}>walk</button>
-      <StringBoard height={boardHeight} width={boardWidth} paths={[walkResult]} />
+      <div>
+        <input value={input} onChange={(event) => setInput(event.target.value)} />
+      </div>
+      <div>
+        <label>Height</label>
+        <input
+          type="number"
+          value={boardHeight}
+          min={1}
+          onChange={(event) => setBoardHeight(Number(event.target.value))}
+        />
+      </div>
+      <div>
+        <label>Width</label>
+        <input
+          type="number"
+          value={boardWidth}
+          min={1}
+          onChange={(event) => setBoardWidth(Number(event.target.value))}
+        />
+      </div>
+
+      <StringBoard {...stringBoardProps} />
     </div>
   );
 }
