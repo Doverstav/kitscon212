fa83a561b65f5282b1a79c0f7440f770c2a1ddbc
Pontus Doverstav <doverstav@gmail.com>
2021-10-04 22:30:46 +0200

    Add type for WalkFunction

--- src/pieces/types.ts
+++ src/pieces/types.ts
@@ -1,3 +1,5 @@
+export type WalkFunction = (params: WalkParams) => Path
+
 export type WalkParams = { boardHeight: number, boardWidth: number, input: string }
 export type Path = Step[]
 export type Step = { x: number, y: number }
