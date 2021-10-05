e6918cd537efd87f750d6bfc8f6b498109eebcb1
Pontus Doverstav <doverstav@gmail.com>
2021-10-05 23:46:02 +0200

    Implement method for extracting bit triplets
    This method can be used for pieces that require three bits for their
    moves, i.e. pieces with eight possible moves (like a knight or a king).

--- src/pieces/helpers.ts
+++ src/pieces/helpers.ts
@@ -8,7 +8,7 @@ export function bitPairs(input: string): string[] {
 
   let bitPairsInOrderOfProcessing: string[] = []
 
-  for (let i = 0; i < binaryArray.length; i = i + 2) {
+  for (let i = 0; (i + 1) < binaryArray.length; i = i + 2) {
     // "Bit pairs are processed bytewise from left to right and least significant bit first"
     // See page 7 here: http://www.dirk-loss.de/sshvis/drunken_bishop.pdf
     const bytePart1 = binaryArray[i]
@@ -29,6 +29,40 @@ export function bitPairs(input: string): string[] {
   }
 
   return bitPairsInOrderOfProcessing
+}
+
+export function bitTriplets(input: string): string[] {
+  const hexString = hash(input)
+  const binaryArray = hexStringToBinary(hexString)
+
+  let bitTriplets: string[] = []
+
+  // (i + 2) < binaryArray.length will aggressively discard any incomplete sequence of 4 triplets
+  // This could potentially throw away 11 bits, so this should probably check for any complete triplets left
+  // and try to extract those.
+  for(let i = 0; (i + 2) < binaryArray.length; i = i + 3) {
+    // Since we want triplets, the closets number divisible by 3 is 12
+    // So we have to take 3 characters at a time
+    // Then we follow the same logic as the orignal algorithm
+    // So bit triplets are processed "byte"-wise from left to right,
+    // and least significant bit first
+    const byteAndAHalf = binaryArray[i].concat(binaryArray[i + 1], binaryArray[i + 2])
+
+    const bitTriplet1 = byteAndAHalf.substring(0, 3)
+    const bitTriplet2 = byteAndAHalf.substring(3, 6)
+    const bitTriplet3 = byteAndAHalf.substring(6, 9)
+    const bitTriplet4 = byteAndAHalf.substring(9, 12)
+
+    bitTriplets = [
+      ...bitTriplets,
+      bitTriplet1,
+      bitTriplet2,
+      bitTriplet3,
+      bitTriplet4,
+    ]
+  }
+
+  return bitTriplets
 }
 
 function hash(input: string): string {
