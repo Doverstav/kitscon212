3cd5ef048aa59de958dd4bb0ce7f72a17e34f0e9
Pontus Doverstav <doverstav@gmail.com>
2021-10-10 21:58:22 +0200

    Fix fatal bug, format files

--- src/board/CanvasBoard.tsx
+++ src/board/CanvasBoard.tsx
@@ -9,49 +9,50 @@ export function CanvasBoard({ height, width, paths }: BoardProps) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    const savedRef = canvasRef.current;
-    const tempBoard: number[][] = Array(height)
-      .fill(null)
-      .map(() => Array(width).fill(0));
-    const pathLength = paths[0].length;
+    if (height > 0 && width > 0) {
+      const savedRef = canvasRef.current;
+      const tempBoard: number[][] = Array(height)
+        .fill(null)
+        .map(() => Array(width).fill(0));
+      const pathLength = paths[0].length;
 
-    const rowLines = height - 1;
-    const rowHeight = CANVAS_HEIGHT / height;
-    const columnLines = width - 1;
-    const columnWidth = CANVAS_WIDTH / width;
+      const rowLines = height - 1;
+      const rowHeight = CANVAS_HEIGHT / height;
+      const columnLines = width - 1;
+      const columnWidth = CANVAS_WIDTH / width;
 
-    console.log(canvasRef.current);
+      console.log(canvasRef.current);
 
-    for (let i = 0; i < pathLength; i++) {
-      paths.forEach((path) => {
-        const currentStep = convertStepOriginFromBottomLeftToTopLeft(
-          path[i],
-          height,
-          width
-        );
-        if (i === 0) {
-          tempBoard[currentStep.y][currentStep.x] = 15;
-        }
-        if (i === pathLength - 1) {
-          tempBoard[currentStep.y][currentStep.x] = 16;
-        }
+      for (let i = 0; i < pathLength; i++) {
+        paths.forEach((path) => {
+          const currentStep = convertStepOriginFromBottomLeftToTopLeft(
+            path[i],
+            height,
+            width
+          );
+          if (i === 0) {
+            tempBoard[currentStep.y][currentStep.x] = 15;
+          }
+          if (i === pathLength - 1) {
+            tempBoard[currentStep.y][currentStep.x] = 16;
+          }
 
-        tempBoard[currentStep.y][currentStep.x] =
-          tempBoard[currentStep.y][currentStep.x] < 15 // Do not overwrite special start/end characters
-            ? tempBoard[currentStep.y][currentStep.x] + 1
-            : tempBoard[currentStep.y][currentStep.x];
-      });
-    }
+          tempBoard[currentStep.y][currentStep.x] =
+            tempBoard[currentStep.y][currentStep.x] < 15 // Do not overwrite special start/end characters
+              ? tempBoard[currentStep.y][currentStep.x] + 1
+              : tempBoard[currentStep.y][currentStep.x];
+        });
+      }
 
-    if (savedRef) {
-      const context = savedRef.getContext("2d");
+      if (savedRef) {
+        const context = savedRef.getContext("2d");
 
-      if (context) {
-        context.fillStyle = "rgb(256,256,256)";
-        context.fillRect(0, 0, savedRef.width, savedRef.height);
-        context.beginPath();
+        if (context) {
+          context.fillStyle = "rgb(256,256,256)";
+          context.fillRect(0, 0, savedRef.width, savedRef.height);
+          context.beginPath();
 
-        /* // Create rowlines
+          /* // Create rowlines
         for(let i = 1; i <= rowLines; i++) {
           context.moveTo(0, rowHeight * i)
           context.lineTo(CANVAS_WIDTH, rowHeight * i)
@@ -64,7 +65,7 @@ export function CanvasBoard({ height, width, paths }: BoardProps) {
           context.stroke()
         } */
 
-        /* for(let i = 0; i < pathLength; i++) {
+          /* for(let i = 0; i < pathLength; i++) {
           context.fillStyle = "rgb(0,0,0)"
           paths.forEach(path => {
             const currentStep = convertStepOriginFromBottomLeftToTopLeft(path[i], height, width)
@@ -72,65 +73,70 @@ export function CanvasBoard({ height, width, paths }: BoardProps) {
             context.fillRect(currentStep.x * columnWidth, currentStep.y * rowHeight, columnWidth, rowHeight)
           })
         } */
-        let lowestValue: number = -1;
-        let highestValue: number = -1;
-        for (let y = 0; y < tempBoard.length; y++) {
-          for (let x = 0; x < tempBoard[y].length; x++) {
-            const currentValue = tempBoard[y][x]
-            if (currentValue !== 0 && currentValue !== 15 && currentValue !== 16) {
-              if(lowestValue === -1 || currentValue < lowestValue ) {
-                lowestValue = currentValue
-              }
+          let lowestValue: number = -1;
+          let highestValue: number = -1;
+          for (let y = 0; y < tempBoard.length; y++) {
+            for (let x = 0; x < tempBoard[y].length; x++) {
+              const currentValue = tempBoard[y][x];
+              if (
+                currentValue !== 0 &&
+                currentValue !== 15 &&
+                currentValue !== 16
+              ) {
+                if (lowestValue === -1 || currentValue < lowestValue) {
+                  lowestValue = currentValue;
+                }
 
-              if(highestValue === -1 || currentValue > highestValue) {
-                highestValue = currentValue
+                if (highestValue === -1 || currentValue > highestValue) {
+                  highestValue = currentValue;
+                }
               }
             }
           }
-        }
 
-        const lightnessStep = (80 - 20) / highestValue
+          const lightnessStep = (80 - 20) / highestValue;
 
-        console.log('lowestValue', lowestValue)
-        console.log('highestValue', highestValue)
+          console.log("lowestValue", lowestValue);
+          console.log("highestValue", highestValue);
 
-        for (let y = 0; y < tempBoard.length; y++) {
-          for (let x = 0; x < tempBoard[y].length; x++) {
-            if (tempBoard[y][x] !== 0) {
-              const lightnessValue = 80 - (lightnessStep * tempBoard[y][x]);
-              //context.fillStyle = `rgb(${colorValue},120,255)`;
-              context.fillStyle = `hsl(149, 53%, ${lightnessValue}%)`
-              context.fillRect(
-                x * columnWidth,
-                y * rowHeight,
-                columnWidth,
-                rowHeight
-              );
-            }
-            if(tempBoard[y][x] === 15) {
-              context.fillStyle = `hsl(149, 53%, 80%)`
-              context.fillRect(
-                x * columnWidth,
-                y * rowHeight,
-                columnWidth,
-                rowHeight
-              );
-            }
-            if(tempBoard[y][x] === 16) {
-              context.fillStyle = `hsl(149, 53%, 10%)`
-              context.fillRect(
-                x * columnWidth,
-                y * rowHeight,
-                columnWidth,
-                rowHeight
-              );
+          for (let y = 0; y < tempBoard.length; y++) {
+            for (let x = 0; x < tempBoard[y].length; x++) {
+              if (tempBoard[y][x] !== 0) {
+                const lightnessValue = 80 - lightnessStep * tempBoard[y][x];
+                //context.fillStyle = `rgb(${colorValue},120,255)`;
+                context.fillStyle = `hsl(149, 53%, ${lightnessValue}%)`;
+                context.fillRect(
+                  x * columnWidth,
+                  y * rowHeight,
+                  columnWidth,
+                  rowHeight
+                );
+              }
+              if (tempBoard[y][x] === 15) {
+                context.fillStyle = `hsl(149, 53%, 80%)`;
+                context.fillRect(
+                  x * columnWidth,
+                  y * rowHeight,
+                  columnWidth,
+                  rowHeight
+                );
+              }
+              if (tempBoard[y][x] === 16) {
+                context.fillStyle = `hsl(149, 53%, 10%)`;
+                context.fillRect(
+                  x * columnWidth,
+                  y * rowHeight,
+                  columnWidth,
+                  rowHeight
+                );
+              }
             }
           }
-        }
 
-        /* context.moveTo(0,0)
+          /* context.moveTo(0,0)
         context.lineTo(CANVAS_WIDTH,CANVAS_HEIGHT)
         context.stroke() */
+        }
       }
     }
   }, [height, width, paths]);
--- src/board/StringBoard.tsx
+++ src/board/StringBoard.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { convertStepOriginFromBottomLeftToTopLeft } from '../pieces/helpers'
-import { Path } from '../pieces/types'
+import React, { useEffect, useState } from "react";
+import { convertStepOriginFromBottomLeftToTopLeft } from "../pieces/helpers";
+import { Path } from "../pieces/types";
 
 export interface BoardProps {
   height: number;
@@ -8,42 +8,70 @@ export interface BoardProps {
   paths: Path[];
 }
 
-const boardCharacters = [' ', '.', 'o', '+', '=', '*', 'B', '0', 'X', '@', '%', '&', '#', '/', '^', 'S', 'E']
+const boardCharacters = [
+  " ",
+  ".",
+  "o",
+  "+",
+  "=",
+  "*",
+  "B",
+  "0",
+  "X",
+  "@",
+  "%",
+  "&",
+  "#",
+  "/",
+  "^",
+  "S",
+  "E",
+];
 
 export const StringBoard: React.FC<BoardProps> = ({ height, width, paths }) => {
-
-  const [boardState, setBoardState] = useState("")
+  const [boardState, setBoardState] = useState("");
 
   useEffect(() => {
-    const tempBoard: number[][] = Array(height).fill(null).map(() => Array(width).fill(0));
-    const pathLength = paths[0].length
+    if (height > 0 && width > 0) {
+      const tempBoard: number[][] = Array(height)
+        .fill(null)
+        .map(() => Array(width).fill(0));
+      const pathLength = paths[0].length;
 
-    for (let i = 0; i < pathLength; i++) {
-      paths.forEach(path => {
-        const currentStep = convertStepOriginFromBottomLeftToTopLeft(path[i], height, width)
-        if (i === 0) {
-          tempBoard[currentStep.y][currentStep.x] = 15
-        }
-        if (i === pathLength - 1) {
-          tempBoard[currentStep.y][currentStep.x] = 16
-        }
+      for (let i = 0; i < pathLength; i++) {
+        paths.forEach((path) => {
+          const currentStep = convertStepOriginFromBottomLeftToTopLeft(
+            path[i],
+            height,
+            width
+          );
+          if (i === 0) {
+            tempBoard[currentStep.y][currentStep.x] = 15;
+          }
+          if (i === pathLength - 1) {
+            tempBoard[currentStep.y][currentStep.x] = 16;
+          }
 
-        tempBoard[currentStep.y][currentStep.x] =
-          tempBoard[currentStep.y][currentStep.x] < 15 // Do not overwrite special start/end characters
-            ? tempBoard[currentStep.y][currentStep.x] + 1
-            : tempBoard[currentStep.y][currentStep.x]
-      })
+          tempBoard[currentStep.y][currentStep.x] =
+            tempBoard[currentStep.y][currentStep.x] < 15 // Do not overwrite special start/end characters
+              ? tempBoard[currentStep.y][currentStep.x] + 1
+              : tempBoard[currentStep.y][currentStep.x];
+        });
+      }
+
+      const paddingRow = ["+", ...Array(width).fill("-"), "+\n"].join("");
+      const content = tempBoard
+        .map((row) => ["|", ...row.map((cell) => boardCharacters[cell]), "|\n"])
+        .flat()
+        .join("");
+
+      setBoardState(paddingRow.concat(content, paddingRow));
     }
-
-    const paddingRow = ['+', ...Array(width).fill('-'), '+\n'].join("");
-    const content = tempBoard.map(row => ['|', ...row.map(cell => boardCharacters[cell]), '|\n']).flat().join("")
-
-    setBoardState(paddingRow.concat(content, paddingRow))
-  }, [paths, height, width])
+  }, [paths, height, width]);
 
   return (
-    <div style={{fontSize: "20px"}}>
+    <div style={{ fontSize: "20px" }}>
       <pre>{boardState}</pre>
     </div>
-  )
-}
+  );
+};
